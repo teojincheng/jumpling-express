fb49200486ffc4fb77662d99894859c2a3eab157
Teo Jin Cheng <jinchengproject@gmail.com>
2020-02-05 22:16:59 +0800

    update tests

--- app.test.js
+++ app.test.js
@@ -38,4 +38,24 @@ describe("app.js", () => {
 
     expect(response.body).toStrictEqual([jumpling]);
   });
+
+  it("GET /jumplings/:id should respond with status 200 and return the jumpling object", async () => {
+    //const jumpling = { id: 3, name: "Tommy" };
+    const response = await request(app)
+      .get("/jumplings/1")
+      .expect(200);
+
+    expect(response.body).toStrictEqual([expectedData[0]]);
+  });
+
+  it("PUT /jumplings/:id should respond with status 200 and return the jumpling object", async () => {
+    const jumpling = { name: "Tommy" };
+    const expectedJumpling = { id: 1, name: "Tommy" };
+    const response = await request(app)
+      .put("/jumplings/1")
+      .expect(200)
+      .send(jumpling);
+
+    expect(response.body).toStrictEqual([expectedJumpling]);
+  });
 });
